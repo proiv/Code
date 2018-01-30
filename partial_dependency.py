4b85154f67496d24d7364be331e3125b05123cf5
proiv <32553360+proiv@users.noreply.github.com>
2018-01-30 08:51:36 -0500

    Create partial_dependency.py
    Partial Dependency function

---
+++ partial_dependency.py
@@ -0,0 +1,27 @@
+def partial_dependency(model, X, y, feature_ids = [], f_id = -1):
+
+    """
+    Calculate the dependency (or partial dependency) of a response variable on a predictor (or multiple predictors)
+    1. Sample a grid of values of a predictor.
+    2. For each value, replace every row of that predictor with this value, calculate the average prediction.
+    """
+
+    X_temp = X.copy()
+
+    grid = np.linspace(np.percentile(X_temp.iloc[:, f_id], 0.1),
+                       np.percentile(X_temp.iloc[:, f_id], 99.5),
+                       50)
+    y_pred = np.zeros(len(grid))
+
+    if len(feature_ids) == 0 or f_id == -1:
+        print ('Input error!')
+        return
+    else:
+        for i, val in enumerate(grid):
+
+            X_temp.iloc[:, f_id] = val
+            data = xgb.DMatrix(X_temp, feature_names = df_columns)
+
+            y_pred[i] = np.average(model.predict(data))
+
+    return grid, y_pred
